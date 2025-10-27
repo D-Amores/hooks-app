741427f14f06870c335ab3a24aef6a971703310a
amores <amores.dev@outlook.com>
2025-10-27 16:34:51 -0600

    feat(useEffect): add countdown side effect with setInterval

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 
 import HooksApp from './HooksApp'
 import { TrafficLight } from './useState/TrafficLight'
+import { TrafficLightWithEffect } from './useEffect/TrafficLightWithEffect'
 
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <HooksApp /> */}
-    <TrafficLight />
+    {/* <TrafficLight /> */}
+    <TrafficLightWithEffect />
   </StrictMode>,
 )
