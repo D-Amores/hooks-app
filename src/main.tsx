7354a8b75484d25698a0f0a4843e5b6d8565dc59
amores <amores.dev@outlook.com>
2025-10-26 19:54:20 -0600

    feat(useState): add TrafficLight component

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import HooksApp from './HooksApp'
+import { TrafficLight } from './useState/TrafficLight'
 
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HooksApp />
+    {/* <HooksApp /> */}
+    <TrafficLight />
   </StrictMode>,
 )
