4647a3fcf94507af784b7cfaf5c29e115a34c886
amores <amores.dev@outlook.com>
2025-11-27 14:17:25 -0600

    refactor(state): begin migrating state logic from useState to useReducer

--- src/useReducer/reducer/ScrambleWordsReducer.ts
+++ src/useReducer/reducer/ScrambleWordsReducer.ts
@@ -10,7 +10,71 @@ export interface ScrambleWordsState {
   skipCounter: number;
   isGameOver: boolean;
   maxSkips: number;
+  totalWords: number;
 }
 
-export type ScrambleWordsAction = 
+const GAME_WORDS = [
+  'REACT',
+  'JAVASCRIPT',
+  'TYPESCRIPT',
+  'HTML',
+  'ANGULAR',
+  'SOLID',
+  'NODE',
+  'VUEJS',
+  'SVELTE',
+  'EXPRESS',
+  'MONGODB',
+  'POSTGRES',
+  'DOCKER',
+  'KUBERNETES',
+  'WEBPACK',
+  'VITE',
+  'TAILWIND',
+];
+
+// Esta función mezcla el arreglo para que siempre sea aleatorio
+const shuffleArray = (array: string[]) => {
+  return array.sort(() => Math.random() - 0.5);
+};
+
+// Esta función mezcla las letras de la palabra
+const scrambleWord = (word: string = '') => {
+  return word
+    .split('')
+    .sort(() => Math.random() - 0.5)
+    .join('');
+};
+
+export const getInitialState = (): ScrambleWordsState => {
+  const shuffleWords = shuffleArray([...GAME_WORDS]);
+
+  return {
+    words: shuffleWords,
+    currentWord: shuffleWords[0],
+    scrambledWord: scrambleWord(shuffleWords[0]),
+    guess: '',
+    points: 0,
+    errorCounter: 0,
+    maxAllowErrors: 3,
+    skipCounter: 0,
+    isGameOver: false,
+    maxSkips: 3,
+    totalWords: shuffleWords.length,
+  }
+}
+
+export type ScrambleWordsAction =
 | { type: 'NO_TENGO_LA_MENOR_IDEA_CUALES_ACCIONES_NECESITO' }
+| { type: 'NO_TENGO_LA_MENOR_IDEA_CUALES_ACCIONES_NECESITO1' }
+| { type: 'NO_TENGO_LA_MENOR_IDEA_CUALES_ACCIONES_NECESITO2' }
+
+export const scrambledWordReducer = ( state: ScrambleWordsState, action: ScrambleWordsAction) => {
+
+  switch (action.type) {
+
+
+    default:
+      return state;
+  }
+}
