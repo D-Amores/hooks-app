bc2832421774274907af3543563e0bfe4c9c6f84
amores <amores.dev@outlook.com>
2025-12-27 23:32:50 -0600

    feat(useContext): implement UserContext for global user information management

--- src/useContext/ProfessionalApp.tsx
+++ src/useContext/ProfessionalApp.tsx
@@ -1,12 +1,15 @@
 
 import { RouterProvider } from 'react-router'
 import { appRouter } from './router/app.router'
+import UserContextProvider from './context/UserContext'
 
 const ProfessionalApp = () => {
   return (
-    <div className='bg-gradient'>
-      <RouterProvider router={appRouter} />
-    </div>
+    <UserContextProvider>
+      <div className='bg-gradient'>
+        <RouterProvider router={appRouter} />
+      </div>
+    </UserContextProvider>
   )
 }
 
---
+++ src/useContext/context/UserContext.tsx
@@ -0,0 +1,42 @@
+import { createContext, useState, type PropsWithChildren } from 'react'
+import type { User } from '../data/user-mock.data';
+
+type AuthStatus = 'authenticated' | 'unauthenticated' | 'checking';
+
+interface UserContextProps {
+  // state
+  authStatus: AuthStatus;
+  user: User | null;
+
+  //methods
+  login: (userId: number) => boolean;
+  logout: () => void;
+}
+
+export const UserContext = createContext({} as UserContextProps);
+
+const UserContextProvider = ({ children }: PropsWithChildren) => {
+  const [authStatus, setAuthStatus] = useState<AuthStatus>('checking');
+  const [user, setUser] = useState<User | null>(null);
+
+  const handleLogin = (userId: number) => {
+    console.log({userId})
+    return true;
+  }
+
+  const handleLogout = () => {
+    console.log('logout');
+  }
+
+  return (
+    <UserContext value={{ 
+        authStatus: authStatus, 
+        user: user,
+        login: handleLogin,
+        logout: handleLogout
+      }}> {children}
+    </UserContext>
+  )
+}
+
+export default UserContextProvider
